1e432b746e990b8b6d50804ae1d87194f8690dea
YH12277 <venkata.jonnabhatla@macys.com>
2019-04-14 22:41:26 -0700

    added integration tests

--- src/test/java/com/example/http/headers/SpringEnvironmentPostPorcessor/SpringEnvironmentPostPorcessorApplicationTests.java
+++ src/test/java/com/example/http/headers/SpringEnvironmentPostPorcessor/SpringEnvironmentPostPorcessorApplicationTests.java
@@ -14,8 +14,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.context.WebApplicationContext;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+//@RunWith(SpringRunner.class)
+//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class SpringEnvironmentPostPorcessorApplicationTests {
 
 	@Autowired
